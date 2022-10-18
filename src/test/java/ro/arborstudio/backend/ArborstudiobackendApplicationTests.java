a3308b0e8401a6121bc5abd5ad477afc0570c437
radubaciuDell <radumbaciu@gmail.com>
2022-10-18 14:17:19 +0300

    initial commit

---
+++ src/test/java/ro/arborstudio/backend/ArborstudiobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.arborstudio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArborstudiobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
